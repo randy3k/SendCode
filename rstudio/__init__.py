9167b195c2810ab4315ae8fbc29926386afe849d
randy3k <randy.cs.lai@gmail.com>
2016-07-16 09:36:15 -0700

    use pywin32 to send code to rstudio on windows

--- rstudio/__init__.py
+++ rstudio/__init__.py
@@ -13,14 +13,29 @@ if plat == "osx":
         osascript(RSTUDIOAPPLESCRIPT, cmd)
 
 elif plat == "windows":
-    from ..autohotkey import autohotkey
-
-    RSTUDIOAHK = os.path.join(os.path.dirname(__file__), "rstudio.ahk")
+    import win32gui
+    import win32api
+    import win32con
+    import win32clipboard
 
     def send_to_rstudio(cmd):
-        clipboard.set_clipboard(cmd)
-        autohotkey(RSTUDIOAHK)
-        clipboard.reset_clipboard()
+        rid = win32gui.FindWindow("Qt5QWindowIcon", "RStudio")
+        # TODO: reset clipboard
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText(cmd)
+        win32clipboard.CloseClipboard()
+        win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+        win32api.PostMessage(rid, win32con.WM_KEYDOWN, ord("V"), 0)
+
+        def do_enter():
+            win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
+            win32api.PostMessage(rid, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+            win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+
+        if rid:
+            sublime.set_timeout(do_enter, 1)
+
 
 elif plat == "linux":
     from xdotool import xdotool
