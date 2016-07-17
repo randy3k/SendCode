fd777882486795280cef52df5afbe96a2f115ac7
randy3k <randy.cs.lai@gmail.com>
2016-07-16 20:21:18 -0700

    add delay between kedboard actions

--- rstudio/__init__.py
+++ rstudio/__init__.py
@@ -17,18 +17,23 @@ elif plat == "windows":
     import win32api
     import win32con
 
+    def delay_action_1(rid):
+        control_state = win32api.GetKeyState(win32con.VK_CONTROL)
+        win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+        win32api.PostMessage(rid, win32con.WM_KEYDOWN, ord("V"), 0)
+        sublime.set_timeout(lambda: delay_action_2(rid, control_state), 1)
+
+    def delay_action_2(rid, control_state):
+        win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
+        win32api.PostMessage(rid, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+        if control_state < 0:
+            sublime.set_timeout(lambda: win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0), 1)
+
     def send_to_rstudio(cmd):
         rid = win32gui.FindWindow("Qt5QWindowIcon", "RStudio")
-        # TODO: reset clipboard
         if rid:
-            control_state = win32api.GetKeyState(win32con.VK_CONTROL)
             clipboard.set_clipboard(cmd)
-            win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
-            win32api.PostMessage(rid, win32con.WM_KEYDOWN, ord("V"), 0)
-            win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
-            win32api.PostMessage(rid, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
-            if control_state < 0:
-                sublime.set_timeout(lambda: win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0), 1)
+            sublime.set_timeout(lambda: delay_action_1(rid), 1)
             clipboard.reset_clipboard()
 
 elif plat == "linux":
