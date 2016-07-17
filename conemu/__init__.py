f3b264d6bdd0217175b114f09a77aa658a5802f1
randy3k <randy.cs.lai@gmail.com>
2016-07-16 17:18:27 -0700

    try to detect conemuc automatically

--- conemu/__init__.py
+++ conemu/__init__.py
@@ -1,6 +1,40 @@
 import sublime
 import os
+import sys
 import subprocess
+import re
+
+
+RE_CMDER = re.compile(r"\"(.*?)\\Cmder\.exe\"")
+RE_CONEMU = re.compile(r"\"(.*?)\\ConEmu(?:64)?\.exe\"")
+
+def cmder_setup():
+    try:
+        akey = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, 
+            "Directory\\shell\\Cmder\command", 0, winreg.KEY_READ)
+        command = winreg.QueryValueEx(akey, "")[0]
+        conemu_base_dir = os.path.join(RE_CMDER.match(command).group(1), "vendor", "conemu-maximus5", "ConEmu")
+        if not conemu_base_dir in os.environ["PATH"]:
+            os.environ["PATH"] = os.environ["PATH"] + ";" + conemu_base_dir
+    except:
+        return
+
+def conemu_setup():
+    try:
+        akey = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, 
+            "Directory\\shell\\ConEmu Here\command", 0, winreg.KEY_READ)
+        command = winreg.QueryValueEx(akey, "")[0]
+        conemu_base_dir = os.path.join(RE_CONEMU.match(command).group(1), "ConEmu")
+        if not conemu_base_dir in os.environ["PATH"]:
+            os.environ["PATH"] = os.environ["PATH"] + ";" + conemu_base_dir
+    except:
+        return
+
+
+if sublime.platform() == "windows":
+    import winreg
+    cmder_setup()
+    conemu_setup()
 
 
 def escape_dquote(cmd):
@@ -17,10 +51,10 @@ def send_to_conemu(cmd, bracketed=False):
         sublime.error_message("ConEmuC.exe not found.")
 
     if bracketed:
-        subprocess.check_call(
+        subprocess.Popen(
             'ConEmuC -GuiMacro:0 Paste(0,"\x1b[200~%s\x1b[201~\n")' % escape_dquote(cmd),
             startupinfo=startupinfo)
     else:
-        subprocess.check_call(
+        subprocess.Popen(
             'ConEmuC -GuiMacro:0 Paste(0,"%s\n")' % escape_dquote(cmd),
             startupinfo=startupinfo)
