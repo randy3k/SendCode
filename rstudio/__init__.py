aa0418538f3278f6b82e771a03dc6cb0929c306e
Randy Lai <randy.cs.lai@gmail.com>
2016-11-17 15:50:39 -0500

    improve windows Rstudio delay

--- rstudio/__init__.py
+++ rstudio/__init__.py
@@ -16,24 +16,22 @@ elif plat == "windows":
     import win32gui
     import win32api
     import win32con
-
-    def delay_action_1(rid):
-        control_state = win32api.GetKeyState(win32con.VK_CONTROL)
-        win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
-        win32api.PostMessage(rid, win32con.WM_KEYDOWN, ord("V"), 0)
-        sublime.set_timeout(lambda: delay_action_2(rid, control_state), 1)
-
-    def delay_action_2(rid, control_state):
-        win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
-        win32api.PostMessage(rid, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
-        if control_state < 0:
-            sublime.set_timeout(lambda: win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0), 1)
+    import time
 
     def send_to_rstudio(cmd):
         rid = win32gui.FindWindow("Qt5QWindowIcon", "RStudio")
         if rid:
             clipboard.set_clipboard(cmd)
-            sublime.set_timeout(lambda: delay_action_1(rid), 1)
+            time.sleep(0.001)
+            win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+            win32api.PostMessage(rid, win32con.WM_KEYDOWN, ord("V"), 0)
+            time.sleep(0.001)
+            control_state = win32api.GetKeyState(win32con.VK_CONTROL)
+            win32api.keybd_event(win32con.VK_CONTROL, 0, win32con.KEYEVENTF_KEYUP, 0)
+            win32api.PostMessage(rid, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+            if control_state < 0:
+                win32api.keybd_event(win32con.VK_CONTROL, 0, 0, 0)
+
             clipboard.reset_clipboard()
 
 elif plat == "linux":
