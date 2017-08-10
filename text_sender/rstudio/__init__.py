207b95c3564942df3150017ce156cf8b95ea5a40
Randy Lai <randy.cs.lai@gmail.com>
2017-08-10 12:03:28 -0400

    don't need release modifiers

--- text_sender/rstudio/__init__.py
+++ text_sender/rstudio/__init__.py
@@ -29,6 +29,6 @@ elif plat == "linux":
         if wid:
             wid = wid.decode("utf-8").strip().split("\n")[-1]
             clipboard.set_clipboard(cmd)
-            xdotool("key", "--window", wid, "--clearmodifiers", "ctrl+v")
+            xdotool("key", "--window", wid, "ctrl+v")
             xdotool("key", "--window", wid, "--clearmodifiers", "Return")
             clipboard.reset_clipboard()
