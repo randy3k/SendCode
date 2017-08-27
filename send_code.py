20554ac3d06d1185b607f883c43cd3da79a7e7d8
Randy Lai <randy.cs.lai@gmail.com>
2017-08-27 16:03:30 -0400

    resolve varaibles using expand_variables

--- send_code.py
+++ send_code.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
-import os
+import re
+
 from .text_getter import TextGetter
 from .text_sender import TextSender
 
@@ -17,49 +18,46 @@ def escape_squote(cmd):
     return cmd
 
 
-def replace_variable(cmd, var, value):
-    cmd = cmd.replace("\"" + var + "\"", "\"" + escape_dquote(value) + "\"")
-    cmd = cmd.replace("'" + var + "'", "'" + escape_squote(value) + "'")
-    return cmd.replace(var, value)
+PATTERN = re.compile(r"""
+    (?P<quote>["'])
+    (?P<quoted_var>
+        \$ (?: [_a-z][_a-z0-9]*  | \{[^}]*\} )
+    )
+    (?P=quote)
+    |
+    (?P<var>
+        \$ (?: [_a-z][_a-z0-9]*  | \{[^}]*\} )
+    )
+""", re.VERBOSE)
 
 
 class SendCodeCommand(sublime_plugin.TextCommand):
 
     def resolve(self, cmd):
         view = self.view
-        file = view.file_name()
-        if file:
-            file_name = os.path.basename(file)
-            file_path = os.path.dirname(file)
-            file_base_name, file_ext = os.path.splitext(file_name)
-            cmd = replace_variable(cmd, "$file_path", file_path)
-            cmd = replace_variable(cmd, "$file_name", file_name)
-            cmd = replace_variable(cmd, "$file_base_name", file_base_name)
-            cmd = replace_variable(cmd, "$file_extension", file_ext)
-            cmd = replace_variable(cmd, "$file", file)
-
+        window = view.window()
+        extracted_variables = window.extract_variables()
         if len(view.sel()) == 1:
             row, _ = view.rowcol(view.sel()[0].begin())
-            cmd = replace_variable(cmd, "$line", str(row+1))
+            extracted_variables["line"] = str(row+1)
 
-        if view.window():
-            pd = view.window().project_data()
-            if pd and "folders" in pd and len(pd["folders"]) > 0:
-                folder = pd["folders"][0].get("path")
-                if folder:
-                    cmd = replace_variable(cmd, "$folder", folder)
-
-            pfn = view.window().project_file_name()
-            if pfn:
-                project_path = os.path.dirname(pfn)
-                cmd = replace_variable(cmd, "$project_path", project_path)
-
-        if len(view.sel()) == 1:
             word = view.substr(view.sel()[0])
             if not word:
                 word = view.substr(view.word(view.sel()[0].begin()))
+            extracted_variables["selection"] = word
 
-            cmd = replace_variable(cmd, "$selection", word)
+        def convert(m):
+            quote = m.group("quote")
+            if quote:
+                var = sublime.expand_variables(m.group("quoted_var"), extracted_variables)
+                if quote == "'":
+                    return "'" + escape_squote(var) + "'"
+                else:
+                    return '"' + escape_dquote(var) + '"'
+            else:
+                return sublime.expand_variables(m.group("var"), extracted_variables)
+
+        cmd = PATTERN.sub(convert, cmd)
 
         return cmd
 
