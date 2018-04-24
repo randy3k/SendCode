d2e368552d221d9ecda1cf2615b7c1ee8254b9f0
Randy Lai <randy.cs.lai@gmail.com>
2018-04-24 11:33:15 -0400

    only for whole line selection

--- code_getter/getter.py
+++ code_getter/getter.py
@@ -167,11 +167,11 @@ class RCodeGetter(CodeGetter):
 
     def substr(self, s):
         view = self.view
-        row = view.rowcol(s.begin())[0]
+        row, col = view.rowcol(s.begin())
         line = view.line(view.text_point(row, 0))
         line_content = view.substr(line)
 
-        if line_content.startswith("#' "):
+        if col == 0 and line_content.startswith("#' "):
             while row >= 0:
                 row = row - 1
                 line = view.line(view.text_point(row, 0))
