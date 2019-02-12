3369771a9342d3198a5e890de9a298282f6cfd75
Randy Lai <randy.cs.lai@gmail.com>
2019-02-12 12:22:21 -0500

    use `paste` command of screen

--- code_sender/screen/__init__.py
+++ code_sender/screen/__init__.py
@@ -6,9 +6,8 @@ def _send_to_screen(cmd, screen):
     n = 200
     chunks = [cmd[i:i+n] for i in range(0, len(cmd), n)]
     for chunk in chunks:
-        chunk = chunk.replace("\\", r"\\")
-        chunk = chunk.replace("$", r"\$")
-        subprocess.check_call([screen, '-X', 'stuff', chunk])
+        subprocess.check_call([screen, '-X', 'register', 'z', chunk])
+        subprocess.check_call([screen, '-X', 'paste', 'z'])
 
 
 def send_to_screen(cmd, screen="screen", bracketed=False, commit=True):
